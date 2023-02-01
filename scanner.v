b1a73136d73b6a768f1a48fba39305cfc236b045
aaron marcus <aaronmarcus2007@gmail.com>
2023-02-01 19:30:00 +0100

    return on error

--- scanner.v
+++ scanner.v
@@ -6,12 +6,19 @@ fn main() {
 	if args.len != 2 {
 		println("\e[1;31mError: Invalid number of arguments\e[0m")
 		println('Usage: ccss <file>')
+		return
 	}
 
 	if !os.exists(args[1]) {
 		println("\e[1;31mError: File does not exist\e[0m")
+		return
 	}
 	
 	mut source := os.read_file(args[1])!
-	println(source)
+	
+	for line in source.split_into_lines() {
+		if line.starts_with('#') {
+			continue
+		}
+	}
 }
